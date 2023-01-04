35b2d1506d236354cd6a3037688ee4c0c63afe93
Benjamin Cash <bcash@login4.frontera.tacc.utexas.edu>
2023-01-04 16:46:27 -0600

    Fix mismatch in template names

--- bin/generate_model_files_from_templates.sh
+++ bin/generate_model_files_from_templates.sh
@@ -7,6 +7,6 @@ CDATE=${CYLC_TASK_PARAM_ldate:0:8}
 export ISEED_CA=$(( (CDATE*1000 + MNUM*10 + 4) % 2147483647 ))
 echo $RUNDIR
 
-python $WORK/workflow-tools/src/uwtools/atparse_to_jinja2.py -i $template -o $RUNDIR/${output}.jinja2
+python $WORK/workflow-tools/src/uwtools/atparse_to_jinja2.py -i $input -o $RUNDIR/${output}.jinja2
 python $WORK/workflow-tools/src/uwtools/templater.py -i $RUNDIR/${output}.jinja2 -o $RUNDIR/${output}
  
