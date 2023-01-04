a1b1cfdbbf96e4fa9ba9e79750f9720f1457e95c
Benjamin Cash <bcash@login3.frontera.tacc.utexas.edu>
2023-01-04 16:21:06 -0600

    Initial commit of setup scripts

---
+++ bin/establish_run_directory.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+MEMBER=$CYLC_TASK_PARAM_mem
+RUNDIR=$RUN_ROOT_DIR/$CYLC_TASK_PARAM_ldate/$RESOL/$MEMBER
+
+echo "path variables"
+echo $RUNDIR
+
+# Check for existing run directory
+if [ -d $RUNDIR ]; then
+    echo $CLEAN_FLAG
+    if [ $CLEAN_FLAG ]; then
+        echo "Removing existing run directory"
+        rm -rf ${RUNDIR}.del
+        mv $RUNDIR ${RUNDIR}.del
+    else
+        echo "Run directory exists and CLEAN_FLAG false, exiting"
+        exit 1
+    fi
+fi
+SUBDIRS=("INPUT" "MOM6_OUTPUT" "MOM6_RESTART" "RESTART" "history" "restart_wave")
+echo $SUBDIRS
+
+for SUBDIR in ${SUBDIRS[@]}; do
+	echo "WTF?"
+	echo $RUNDIR/$SUBDIR 
+    if ! [ -e $RUNDIR/SUBDIR ]; then
+        mkdir -p $RUNDIR/$SUBDIR
+    fi
+done
---
+++ bin/generate_model_files_from_templates.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash -f
+
+MEMBER=$CYLC_TASK_PARAM_mem
+RUNDIR=$RUN_ROOT_DIR/$CYLC_TASK_PARAM_ldate/$RESOL/$MEMBER
+MNUM=10#${MEMBER:3:3}
+CDATE=${CYLC_TASK_PARAM_ldate:0:8}
+export ISEED_CA=$(( (CDATE*1000 + MNUM*10 + 4) % 2147483647 ))
+echo $RUNDIR
+
+python $WORK/workflow-tools/src/uwtools/atparse_to_jinja2.py -i $template -o $RUNDIR/${output}.jinja2
+python $WORK/workflow-tools/src/uwtools/templater.py -i $RUNDIR/${output}.jinja2 -o $RUNDIR/${output}
+ 
