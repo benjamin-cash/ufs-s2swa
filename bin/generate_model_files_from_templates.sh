b4a3ca865a900b5db04ebee19eb7d4852641297a
Benjamin Cash <bcash@login2.frontera.tacc.utexas.edu>
2023-01-05 11:58:17 -0600

    Updates to the harness to allow for easier generation of required variable list

--- bin/generate_model_files_from_templates.sh
+++ bin/generate_model_files_from_templates.sh
@@ -23,7 +23,6 @@ CDATE=${CYLC_TASK_PARAM_ldate:0:8}
 MNUM=10#${MEMBER:3:3}
 export ISEED_CA=$(( (CDATE*1000 + MNUM*10 + 4) % 2147483647 ))
 
-
 python $WORK/workflow-tools/src/uwtools/atparse_to_jinja2.py -i $input -o $RUNDIR/${output}.jinja2
 python $WORK/workflow-tools/src/uwtools/templater.py -i $RUNDIR/${output}.jinja2 -o $RUNDIR/${output}
  
--- bin/harness_generate_model_files_from_templates.sh
+++ bin/harness_generate_model_files_from_templates.sh
@@ -4,7 +4,8 @@ export CYLC_TASK_PARAM_mem="mem001"
 export RUN_ROOT_DIR="/scratch1/02441/bcash/prototype-p8/ufs-s2swa.test.744.x12.y16.cpn36.wg3.wt108/run"
 export CYLC_TASK_PARAM_ldate=2012010100
 export RESOL="C384"
-export input="/work2/02441/bcash/frontera/ufs-weather-model/prototype-p8/tests/parm/model_configure.IN"
-export output="model_configure"
+export input="/work2/02441/bcash/frontera/ufs-weather-model/prototype-p8/tests/parm/nems.configure.cpld.IN"
+export output="nems.configure"
 
 bash generate_model_files_from_templates.sh
+python $WORK/workflow-tools/src/uwtools/templater.py -i $RUN_ROOT_DIR/$CYLC_TASK_PARAM_ldate/$RESOL/$CYLC_TASK_PARAM_mem/${output}.jinja2 --values_needed
