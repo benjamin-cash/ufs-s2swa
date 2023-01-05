f88494bfd526f0cc9236091d552ef11204b28e09
Benjamin Cash <bcash@login2.frontera.tacc.utexas.edu>
2023-01-05 10:59:17 -0600

    Changes to allow for generation of model_configure

--- bin/generate_model_files_from_templates.sh
+++ bin/generate_model_files_from_templates.sh
@@ -2,10 +2,27 @@
 
 MEMBER=$CYLC_TASK_PARAM_mem
 RUNDIR=$RUN_ROOT_DIR/$CYLC_TASK_PARAM_ldate/$RESOL/$MEMBER
-MNUM=10#${MEMBER:3:3}
-CDATE=${CYLC_TASK_PARAM_ldate:0:8}
-export ISEED_CA=$(( (CDATE*1000 + MNUM*10 + 4) % 2147483647 ))
 echo $RUNDIR
+
+# Date manipulation to generate environment variables to fill in template
+# from cylc ldate
+export SYEAR=${CYLC_TASK_PARAM_ldate:0:4}
+export SMONTH=${CYLC_TASK_PARAM_ldate:4:2}
+export SDAY=${CYLC_TASK_PARAM_ldate:6:2}
+export SHOUR=${CYLC_TASK_PARAM_ldate:8:2}
+
+# CYCLE manipulation to calculate start and ending hours
+export FHROT=$(( (CYLC_TASK_CYCLE_POINT-1)*NHOURS ))
+
+# Increment in hours for end of next cycle
+export FHMAX=$(( (CYLC_TASK_CYCLE_POINT)*NHOURS ))
+echo $CYLC_TASK_CYCLE_POINT $CYLC_NHOURS
+
+# Calculate ISEED_CA based on ldate and ensemble member
+CDATE=${CYLC_TASK_PARAM_ldate:0:8}
+MNUM=10#${MEMBER:3:3}
+export ISEED_CA=$(( (CDATE*1000 + MNUM*10 + 4) % 2147483647 ))
+
 
 python $WORK/workflow-tools/src/uwtools/atparse_to_jinja2.py -i $input -o $RUNDIR/${output}.jinja2
 python $WORK/workflow-tools/src/uwtools/templater.py -i $RUNDIR/${output}.jinja2 -o $RUNDIR/${output}
